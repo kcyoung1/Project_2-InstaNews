f87676a6fa8916a3f050f8d3bf67c26f1f78f062
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-05-01 16:58:14 -0700

    added 1000px screen size media query

--- src/main.js
+++ src/main.js
@@ -24,23 +24,19 @@ $(function() {
                 url: 'http://api.nytimes.com/svc/topstories/v1/world.json?api-key=79cdc4c15ba80dc0b637d0b5c9ca165f:2:75124069',
             })
             .done(function(data) {
-                // console.log(data);
-                // console.log(data.results);
+
                 var nytData = data.results;
                 nytData = nytData.filter(function(item) {
                     return item.multimedia.length;
                 }).splice(0, 12);
 
                 nytData.forEach(function(item, index) {
-                    // console.log(item, index);
-                    $('.newsarticles').append('<div class="allart article-' + index + '"><div class="text"><a href="' + item.url + '"> ' + item.abstract + '</a></div></div>');
+
+                    $('.news-articles').append('<div class="all-articles article-' + index + '"><div class="text"><a href="' + item.url + '"> ' + item.abstract + '</a></div></div>');
 
                     img = item.multimedia[4];
                     $('.article-' + index).css('background-image', 'url("' + img.url + '")');
-                    // console.log(value.abstract);
-                    //     console.log(value.url);
-                    //     console.log(value.title);
-                    //
+
                 });
             });
     });
