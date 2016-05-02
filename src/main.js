2c612187384058b628c408f8ff3eddc72a5ab73f
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-05-02 16:31:25 -0700

    added if/else statement to get the sorry error

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 $(function() {
     $('select').on('change', function() {
         event.preventDefault();
+
+        // Change header
         $('header').switchClass("head-large", "head-small", 1000, "easeOutQuad");
         $('img').switchClass("logo-large", "logo-small", 1000, "easeOutQuad");
 
+        // Loader
         $('.news-articles').empty();
         $('.loading').show();
 
@@ -14,12 +17,14 @@ $(function() {
                 url: 'http://api.nytimes.com/svc/topstories/v1/' + selection + '.json?api-key=79cdc4c15ba80dc0b637d0b5c9ca165f:2:75124069',
             })
             .done(function(data) {
+                if (data.results.length === 0) {
+                    $('.news-articles').append('<p>Sorry, nothing found. Please try another section.</p>');
+                } else {
+                    var nytData = data.results;
+                    nytData = nytData.filter(function(item) {
+                        return item.multimedia.length;
+                    }).splice(0, 12);
 
-                var nytData = data.results;
-                nytData = nytData.filter(function(item) {
-                    return item.multimedia.length;
-                }).splice(0, 12);
-                if (data.resultCount !== 0) {
                     nytData.forEach(function(item, index) {
 
                         $('.news-articles').append('<div class="all-articles article-' + index + '"><div class="text"><a href="' + item.url + '"> ' + item.abstract + '</a></div></div>');
@@ -28,9 +33,8 @@ $(function() {
                         $('.article-' + index).css('background-image', 'url("' + img.url + '")');
 
                     });
-                } else {
-                    $('.news-articles').append('<p>Sorry, you can\'t spell properly.</p>');
                 }
+
             }).always(function() {
                 $('.loading').hide();
             });
